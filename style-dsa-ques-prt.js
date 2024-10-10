ec9e8ccd8494e26f4015f861aa0682f700dd4b44
AditiAditya <163454278+AditiAditya@users.noreply.github.com>
2024-10-11 02:16:19 +0530

    DSA THEORY PRACTICE

---
+++ style-dsa-ques-prt.js
@@ -0,0 +1,25 @@
+// Fibonacci sequence 
+// 0,1,2,3,5,8,13,21,34,55,and so on...
+
+// let a = 0,b = 1;
+// console.log(a);
+// console.log(b);
+// for (let i = 0; i <= 10 ; i++) {
+//  var temperyNumber = a + b ;
+//     console.log(temperyNumber);
+//     a = b;
+//     b = temperyNumber;
+
+// }
+
+
+
+// Factorial of a number 
+// eg :- fact. of 5 is  5*4*3*2*1 = 120 
+
+let givenNo = prompt("Give me a Number ")
+let fact = 1;
+
+if (givenNo == 0 ) {
+    console.log(`Factorial of  ${givenNo} is 1 `);
+}
